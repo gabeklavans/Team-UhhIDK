6b0b3eb6e1dc2c1bcdc4624656c6bb5992eeebdf
Gabriel Klavans <gabeklav@bu.edu>
2019-12-05 23:32:03 -0500

    Made jshint declaration better

--- backend/routes/coolclimate_api.js
+++ backend/routes/coolclimate_api.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 const express = require("express");
 const rp = require("request-promise");
 const router = express.Router();
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
